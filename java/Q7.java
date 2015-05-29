303fe2b63a11205a2e88a43c661ad48cc9ffd4b4
Dylan Vann <dylan.vann@gmail.com>
2015-05-29 02:23:44 -0400

    Add Q7.

---
+++ java/Q7.java
@@ -0,0 +1,29 @@
+public class Q7 {
+
+    /**
+     * If an element in the matrix is zero, zero the entire row and column.
+     *
+     * @param mat The matrix.
+     */
+    public static void setZeros(int[][] mat) {
+        boolean[] zeroRows = new boolean[mat.length];
+        boolean[] zeroCols = new boolean[mat[0].length];
+        for (int i = 0 ; i < mat.length ; i++) {
+            for (int j = 0 ; j < mat[0].length ; j++) {
+                if (mat[i][j] == 0) {
+                    zeroRows[i] = true;
+                    zeroCols[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0 ; i < mat.length ; i++) {
+            for (int j = 0 ; j < mat[0].length ; j++) {
+                if (zeroRows[i] || zeroCols[j]) {
+                    mat[i][j] = 0;
+                }
+            }
+        }
+    }
+
+}
---
+++ java/Q7Test.java
@@ -0,0 +1,49 @@
+import CtCILibrary.AssortedMethods;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q7Test {
+
+    @Test
+    public void testSetZeros() throws Exception {
+        int[][] input = {
+                {4, 0, 8, 3},
+                {1, 2, 1, 7},
+                {1, 3, 2, 0},
+                {5, 8, 3, 6}
+        };
+        int[][] expectedOutput = {
+                {0, 0, 0, 0},
+                {1, 0, 1, 0},
+                {0, 0, 0, 0},
+                {5, 0, 3, 0}
+        };
+        int[][] output = {
+                {4,0,8,3},
+                {1,2,1,7},
+                {1,3,2,0},
+                {5,8,3,6}
+        };
+
+        Q7.setZeros(output);
+
+        System.out.println("Input:");
+        AssortedMethods.printMatrix(input);
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Expected output:");
+        AssortedMethods.printMatrix(expectedOutput);
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Output:");
+        AssortedMethods.printMatrix(output);
+        System.out.println();
+        System.out.println();
+
+        assertArrayEquals(expectedOutput, output);
+    }
+
+}
