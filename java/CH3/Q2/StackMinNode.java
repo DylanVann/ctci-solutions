42e8eb25a672b1017239923330cb23c83bffaa31
Dylan Vann <dylan.vann@gmail.com>
2015-05-31 23:24:13 -0400

    Add CH3/Q2.

---
+++ java/CH3/Q2/StackMinNode.java
@@ -0,0 +1,10 @@
+package CH3.Q2;
+
+public class StackMinNode {
+    public int value;
+    public int min;
+    public StackMinNode(int value, int min) {
+        this.value = value;
+        this.min = min;
+    }
+}
