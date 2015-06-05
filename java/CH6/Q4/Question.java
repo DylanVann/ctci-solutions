d41a3741eaddc9afa093d84d2204361d5b47d366
Dylan Vann <dylan.vann@gmail.com>
2015-06-05 14:38:02 -0400

    Add CH6.

---
+++ java/CH6/Q4/Question.java
@@ -0,0 +1,42 @@
+package CH6.Q4;
+
+public class Question {
+
+    /**
+     * ##Question 4.4
+     *
+     * A bunch of men are on an island. A genie comes down and gathers everyone
+     * together and places a magical hat on some people’s heads (i.e., at least
+     * one person has a hat). The hat is magical: it can be seen by other people,
+     * but not by the wearer of the hat himself. To remove the hat, those (and
+     * only those who have a hat) must dunk themselves underwater at exactly
+     * midnight. If there are n people and c hats, how long does it take the men
+     * to remove the hats? The men cannot tell each other (in any way) that they
+     * have a hat.
+     */
+
+    /**
+     *
+     * ###Solution
+     *
+     * **Case c=1:**
+     *
+     * Person with the hat on sees no other hats, goes the first night.
+     *
+     * **Case c=n:**
+     *
+     * *A person with a hat on:*
+     *
+     * 1. Sees n-1 hats.
+     * 2. Is unsure whether c is n-1 or n.
+     * 3. Seeing that hats are still there on day n-1 goes on night n.
+     *
+     * *A person without a hat on:*
+     *
+     * 1. Sees n hats.
+     * 2. Is unsure whether c is n or n+1.
+     * 3. Waits until night n, and sees all hats come off, so they do not interfere with the process.
+     *
+     */
+
+}
---
+++ java/CH6/Q6/Question.java
@@ -0,0 +1,38 @@
+package CH6.Q6;
+
+public class Question {
+
+    /**
+     * There are 100 closed lockers in a hallway.
+     *
+     * 1. All lockers are opened.
+     * 2. Every second locker is toggled.
+     * 3. Every third locker is toggled.
+     * ...
+     * 100. The 100th locker is toggled.
+     *
+     * Which lockers will be open?
+     */
+
+    /**
+     * **When is a door toggled?**
+     *
+     * The nth door is toggled once for each factor of n.
+     *
+     * ex. Door 15 is toggled on rounds: 1,3,5,15.
+     *
+     * In the end doors that are toggled an even number of times will be closed.
+     * So doors with an even number of factors (ex. 15) will be closed.
+     * So doors with an odd number of factors will be open.
+     *
+     * Which doors have an odd number of factors?
+     *
+     * Doors that are perfect squares.
+     * ex. 16, with factors: (1,16),(2,8),(4,4)
+     * That's 5 factors.
+     *
+     * Perfect squares that are less than 100:
+     * 10*10,9*9,8*8,...,1
+     */
+
+}
