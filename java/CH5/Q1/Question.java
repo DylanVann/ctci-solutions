ec4adf4da8a209bdf51270172213d4936b696473
Dylan Vann <dylan.vann@gmail.com>
2015-06-13 20:30:36 -0400

    Add doc.

--- java/CH5/Q1/Question.java
+++ java/CH5/Q1/Question.java
@@ -2,6 +2,24 @@ package CH5.Q1;
 
 public class Question {
 
+    /**
+     * Take two integers a and b.
+     * Write the bits from b into a in between indices from and to.
+     *
+     * ex.
+     * a = 111111
+     * b = 000000
+     * indices:
+     * from = 2
+     * to = 4
+     * result = 001110
+     *
+     * @param a First int.
+     * @param b Second int.
+     * @param from Start index.
+     * @param to End index.
+     * @return Combined int.
+     */
     int writeBits(int a, int b, int from, int to) {
         int max = ~0;
 
