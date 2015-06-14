1dd6d529352aab164c688a421946f3a916dd6181
Dylan Vann <dylan.vann@gmail.com>
2015-06-13 20:25:22 -0400

    Explain testing procedure.

--- java/CH1/Q3/QuestionTest.java
+++ java/CH1/Q3/QuestionTest.java
@@ -6,10 +6,27 @@ import static org.junit.Assert.*;
 
 public class QuestionTest {
 
+    /**
+     * This is something I'll do often in my tests.
+     *
+     * Since we're testing multiple implementations of the same function we can define an interface for the function.
+     *
+     */
     public interface RemoveDuplicateCharacters {
         String removeDuplicateCharacters(String str);
     }
 
+    /**
+     * We have one function that performs tests for multiple implementations of a function.
+     *
+     * We pass in the specific function to test:
+     *
+     * ```
+     * testRemoveDuplicateCharacters(Question::removeDuplicateCharacters);
+     * ```
+     *
+     * @param removeFunction The function to test for removeDuplicateCharacters functionality.
+     */
     public void testRemoveDuplicateCharacters(RemoveDuplicateCharacters removeFunction) {
         assertEquals("abcdefg", removeFunction.removeDuplicateCharacters("abccdeefg"));
         assertEquals("abcd", removeFunction.removeDuplicateCharacters("abcd"));
