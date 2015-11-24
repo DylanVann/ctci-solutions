2925d93eb91fb4c66b3e900dcf1170bb8075ef92
Dylan Vann <dylan.vann@gmail.com>
2015-11-24 12:00:09 -0500

    Move test code into test class.

--- java/CH9_SortingAndSearching/Q4_LargeFileSort.java
+++ java/CH9_SortingAndSearching/Q4_LargeFileSort.java
@@ -1,10 +1,7 @@
 package CH9_SortingAndSearching;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Q4_LargeFileSort {
 
@@ -16,44 +13,7 @@ public class Q4_LargeFileSort {
      * 1. Divide the file into reasonably sized chunks.
      * 2. Sort the chunks individually, saving back to the file.
      * 3. Merge the chunks.
-     *
-     * Testing
-     *
-     * To actually test this we're first going to need a big text file.
-     *
-     * 1. Using a word list from http://app.aspell.net/.
-     * word_list_original.txt
-     *
-     * 2. Duplicate each line in the file 300 times to get the file size from ~7mb up to 2GB.
-     * word_list_expected_output.txt
-     * At this point the word list is already sorted.
-     *
-     * 3. Create a new file that is a scrambled version of this list, with the lines rearranged randomly.
-     * word_list_scrambled.txt
-     *
-     * 4. Apply the sorting algorithm to this new file.
-     * word_list_output.txt
-     *
-     * 5. Compare word_list_expected_output.txt to word_list_output.txt
-     * to determine if the algorithm produced the correct output.
      */
-
-    public static void test() throws IOException {
-        duplicateLines("Q4_word_list_original.txt", 300, "Q4_word_list_expected_output.txt");
-        scrambleFile("Q4_word_list_expected_output.txt", "Q4_word_list_scrambled.txt");
-        sortLargeFile("Q4_word_list_scrambled.txt", "Q4_word_list_output.txt");
-    }
-
-    public static void duplicateLines(String inputFileName, int times, String outputFileName) {
-        File input = new File(inputFileName);
-        File output = new File(outputFileName);
-    }
-
-    public static void scrambleFile(String inputFileName, String outputFileName) {
-        File input = new File(inputFileName);
-        File output = new File(outputFileName);
-    }
-
     public static void sortLargeFile(String inputFileName, String outputFileName) throws IOException {
         File inputFile = new File(inputFileName);
         File outputFile = new File(outputFileName);
@@ -62,44 +22,22 @@ public class Q4_LargeFileSort {
         for (File f : splitFiles) sortFile(f);
     }
 
+    /**
+     * @param file
+     * @param blockSize
+     * @return
+     * @throws IOException
+     */
     public static List<File> splitFile(File file, long blockSize) throws IOException {
         List<File> files = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new FileReader(file));
-        try {
-            List<String> lines = new ArrayList<>();
-            String line = "";
-                while (line != null) {
-                    long currentBlockSize = 0;
-                    while ((currentBlockSize < blockSize)) {
-                        line = reader.readLine();
-                        if (line == null) break;
-                        lines.add(line);
-                        currentBlockSize += line.length();
-                    }
-                    File tmpFile = File.createTempFile("SplitFile", "file");
-                    BufferedWriter writer = new BufferedWriter(new FileWriter(tmpFile));
-                    for (String s : lines) {
-                        writer.write(s);
-                        writer.newLine();
-                    }
-                    files.add(tmpFile);
-                    lines.clear();
-                }
-        }
-        finally {
-            reader.close();
-        }
-        return files;
-    }
-
-    public static void sortFile(File file) throws IOException {
         List<String> lines = new ArrayList<>();
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        try {
-            String line = reader.readLine();
-            while (line != null) {
+        String line;
+        while ((line = reader.readLine()) != null) {
+            long currentBlockSize = 0;
+            while ((currentBlockSize < blockSize)) {
                 lines.add(line);
-                line = reader.readLine();
+                currentBlockSize += line.length();
             }
             File tmpFile = File.createTempFile("SplitFile", "file");
             BufferedWriter writer = new BufferedWriter(new FileWriter(tmpFile));
@@ -107,9 +45,107 @@ public class Q4_LargeFileSort {
                 writer.write(s);
                 writer.newLine();
             }
+            files.add(tmpFile);
+            lines.clear();
         }
-        finally {
-            reader.close();
+        reader.close();
+        return files;
+    }
+
+    /**
+     * 1. Create a bufferedReader for each file.
+     * 2. Fill an array with the last line of each file.
+     * 3. Find the min string in that array.
+     * 4. Add the next line from the file that contained that string to the array.
+     * 5. Repeat steps 3 and 4 until all files have been read completely.
+     *
+     * @param files
+     * @param blockSize
+     * @return
+     * @throws IOException
+     */
+    public static File mergeSortFiles(List<File> files, int blockSize) throws IOException {
+        File output = File.createTempFile("MergeSort", ".file");
+        BufferedWriter outputWriter = new BufferedWriter(new FileWriter(output));
+
+        // 1. Create readers.
+        ArrayList<BufferedReader> readers = new ArrayList<>();
+        for (File f : files) {
+            BufferedReader reader = new BufferedReader(new FileReader(f));
+            readers.add(reader);
         }
+
+        // 2. Fill array with the last line of each file.
+        ArrayList<String> lines = new ArrayList<>();
+        for (BufferedReader reader : readers) {
+            String line = reader.readLine();
+            if (line == null) {
+                readers.remove(reader);
+            }
+            else {
+                lines.add(line);
+            }
+        }
+
+        while (readers.size() > 0) {
+            // 3. Find the min string in the array.
+            ArrayList<BufferedReader> emptyReaders = new ArrayList<>();
+            BufferedReader minReader = readers.get(0);
+            String minLine = minReader.readLine();
+            for (int i = 0 ; i < lines.size() ; i++) {
+                BufferedReader lineReader = readers.get(i);
+                String line = lines.get(i);
+                if (line == null) {
+                    emptyReaders.add(lineReader);
+                }
+                else if (line.compareTo(minLine) > 0) {
+                    minReader = lineReader;
+                    minLine = line;
+                }
+            }
+
+            // Remove empty readers.
+            readers.removeAll(emptyReaders);
+            emptyReaders.clear();
+
+            // 4. Add the next line from the file that contained the minString to the array.
+            lines.add(minReader.readLine());
+        }
+
+        BufferedReader minStringReader = readers.get(0);
+        String minString = minStringReader.readLine();
+        assert minString != null;
+        for (BufferedReader reader : readers) {
+            String s = reader.readLine();
+            if (s == null) {
+                readers.remove(reader);
+            }
+            else if (s.compareTo(minString) > 0) {
+                minStringReader = reader;
+                minString = s;
+            }
+        }
+
+        return output;
+    }
+
+    /**
+     * @param file
+     * @throws IOException
+     */
+    public static void sortFile(File file) throws IOException {
+        List<String> lines = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            lines.add(line);
+        }
+        File tmpFile = File.createTempFile("SortedFile", "file");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tmpFile));
+        for (String s : lines) {
+            writer.write(s);
+            writer.newLine();
+        }
+        reader.close();
     }
 }
--- java/CH9_SortingAndSearching/Q4_LargeFileSortTest.java
+++ java/CH9_SortingAndSearching/Q4_LargeFileSortTest.java
@@ -1,5 +1,46 @@
 package CH9_SortingAndSearching;
 
+import org.junit.Test;
+
+import java.io.*;
+import java.util.List;
+
 public class Q4_LargeFileSortTest {
+
+    /**
+     * To actually test this we're first going to need a big text file.
+     *
+     * 1. Using a word list from [http://app.aspell.net/](http://app.aspell.net/).
+     * word_list_original.txt
+     *
+     * 2. Duplicate each line in the file 300 times to get the file size from ~7mb up to 2GB.
+     * word_list_expected_output.txt
+     * At this point the word list is already sorted.
+     *
+     * 3. Create a new file that is a scrambled version of this list, with the lines rearranged randomly.
+     * word_list_scrambled.txt
+     *
+     * 4. Apply the sorting algorithm to this new file.
+     * word_list_output.txt
+     *
+     * 5. Compare word_list_expected_output.txt to word_list_output.txt
+     * to determine if the algorithm produced the correct output.
+     */
+    @Test
+    public void sortLargeFileTest() throws Exception {
+        duplicateLines("Q4_word_list_original.txt", 300, "Q4_word_list_expected_output.txt");
+        scrambleFile("Q4_word_list_expected_output.txt", "Q4_word_list_scrambled.txt");
+        Q4_LargeFileSort.sortLargeFile("Q4_word_list_scrambled.txt", "Q4_word_list_output.txt");
+    }
+
+    public static void duplicateLines(String inputFileName, int times, String outputFileName) {
+        File input = new File(inputFileName);
+        File output = new File(outputFileName);
+    }
+
+    public static void scrambleFile(String inputFileName, String outputFileName) {
+        File input = new File(inputFileName);
+        File output = new File(outputFileName);
+    }
 
 }
